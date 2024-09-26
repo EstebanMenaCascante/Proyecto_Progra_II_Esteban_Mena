b4a024f670c0791aee83f58150789daf70e54f46
EstebanMenaCascante <esteban.mena.cascante@est.una.ac.cr>
2024-09-26 07:28:17 -0600

    Se creo el repositiorio y se configuro SFML

---
+++ Proyecto_Progra_II_Esteban_Mena/Proyecto_Progra_II_Esteban_Mena.cpp
@@ -0,0 +1,10 @@
+// Proyecto_Progra_II_Esteban_Mena.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
